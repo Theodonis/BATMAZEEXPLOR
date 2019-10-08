5ca0cd52aea4aec524f4d241fcd47d32a9a85dd1
Theodonis <theo.zwahlen@mail.ch>
2019-10-08 10:31:36 +0200

    drive faster, log faster

--- MCCAR_V_2_75/Generated_Code/CPU_Config.h
+++ MCCAR_V_2_75/Generated_Code/CPU_Config.h
@@ -6,7 +6,7 @@
 **     Version     : Component 01.046, Driver 01.00, CPU db: 3.00.000
 **     Repository  : Kinetis
 **     Compiler    : GNU C Compiler
-**     Date/Time   : 2019-10-04, 07:42, # CodeGen: 194
+**     Date/Time   : 2019-10-04, 14:52, # CodeGen: 195
 **
 **     Copyright : 1997 - 2015 Freescale Semiconductor, Inc. 
 **     All Rights Reserved.
