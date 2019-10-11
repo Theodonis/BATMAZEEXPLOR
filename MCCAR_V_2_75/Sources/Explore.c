4fa2356bb59ab0578ea6efcf932183b3c921636c
Theodonis <theo.zwahlen@mail.ch>
2019-10-11 17:19:21 +0200

    implementet Segmentend- detection -> Stop at end...

--- MCCAR_V_2_75/Sources/Explore.c
+++ MCCAR_V_2_75/Sources/Explore.c
@@ -8,6 +8,7 @@
 #include "stdbool.h"
 #include "ADC.h"
 #include "Explore.h"
+#define SEGMENT_END_TRESHOLD 0.08
 
 volatile static segEndDet_state_t segEndDetState = FIRST_CALL;
 
@@ -16,6 +17,10 @@ bool segEndDetection(ADC_data_t *adcData,bool *segmentEnd) {
 	static float prev_2_dist[2] 	= { 0.0, 0.0};
 	static float prev_1_dist[2] 	= { 0.0, 0.0};
 	static float dist[2] 			= { 0.0, 0.0};
+	float prevVal;
+	float actualVal;
+	float d_Val;
+	bool segEnd = FALSE;
 
 
 
@@ -45,8 +50,31 @@ bool segEndDetection(ADC_data_t *adcData,bool *segmentEnd) {
 			dist[1] 		= adcData->mm_Values.mm_Left;
 			break;
 	}
+	/* Calculation for rightDistance */
+	prevVal  	= prev_2_dist[0] + prev_1_dist[0];
+	actualVal 	= prev_1_dist[0] + dist[0];
 
-	*segmentEnd =FALSE;
+	d_Val 		= actualVal - prevVal;
+	d_Val 		= d_Val*d_Val;
+
+	if (d_Val>SEGMENT_END_TRESHOLD){
+		segEnd = TRUE;
+	}
+	/* Calculation for leftDistance */
+	prevVal  	= prev_2_dist[1] + prev_1_dist[1];
+	actualVal 	= prev_1_dist[1] + dist[1];
+
+	d_Val 		= actualVal - prevVal;
+	d_Val 		= d_Val*d_Val;
+
+	/* Decide if segmentend detected */
+	if (d_Val>SEGMENT_END_TRESHOLD && segEnd){
+		segEnd = TRUE;
+	}else{
+		segEnd = FALSE;
+	}
+
+	*segmentEnd =segEnd;
 	return TRUE;
 
 
