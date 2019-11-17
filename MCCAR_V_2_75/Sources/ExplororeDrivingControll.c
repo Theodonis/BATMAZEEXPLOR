b346e339bf1ca625ac0d5e6f6613ab36714f27ae
Theodonis <theo.zwahlen@mail.ch>
2019-11-17 13:31:36 +0100

    Fix driving branch, add internal 90 turn to do twice without a reint for 180 turn

--- MCCAR_V_2_75/Sources/ExplororeDrivingControll.c
+++ MCCAR_V_2_75/Sources/ExplororeDrivingControll.c
@@ -117,11 +117,11 @@ byte driveToBranch(uint8_t* segmentNumber, t_dir dir){
 				/*Error but Driving must be finished */
 				state_toBranch=gen_ErrorState;
 				setStopFlag();
-			}else if(dir==left|adc_data.mm_Values.mm_Left>90){
+			}else if(dir==left&&(adc_data.mm_Values.mm_Left>90)){
 				/* Branch detected Finish Driving */
 				state_toBranch = gen_deinitState; // ev. go to wait state for some cycles to be in midle of branch
 				setStopFlag();
-			}else if(dir==right|adc_data.mm_Values.mm_Right>90){
+			}else if(dir==right&&(adc_data.mm_Values.mm_Right>90)){
 				/* Branch detected Finish Driving */
 				state_toBranch = gen_deinitState; // ev. go to wait state for some cycles to be in midle of branch
 				setStopFlag();
@@ -149,25 +149,40 @@ byte driveToBranch(uint8_t* segmentNumber, t_dir dir){
 	return ERR_BUSY;
 }
 
-byte turn90(uint8_t* segmentNumber, t_directions* currentOrientation, t_dir dir){
+
+
+byte turn90Intern(uint8_t* segmentNumber, t_directions* currentOrientation, t_dir dir, bool reinit){
 	static t_genericState state_turn90 = gen_initState;
 	static Maze_segments Maze_seg;
 	ADC_data_t adc_data;
 
 	switch (state_turn90) {
 		case gen_initState:
-			reinit_Drving(true);
-			Maze_seg.numberOfSegments = 1;
-			(*segmentNumber) = 1;
-			if(dir == left){
-				Maze_seg.segments[0].SingleSegment = 900;
-			}else if(dir == right){
-				Maze_seg.segments[0].SingleSegment = -900;
+			if(reinit){
+				reinit_Drving(true);
+
+				Maze_seg.numberOfSegments = 1;
+				(*segmentNumber) = 1;
+				if(dir == left){
+					Maze_seg.segments[0].SingleSegment = 900;
+				}else if(dir == right){
+					Maze_seg.segments[0].SingleSegment = -900;
+				}else{
+					state_turn90 = gen_initState;
+					return ERR_FAILED;
+				}
+				Maze_seg.segments[1].SingleSegment = 0; /* stop after this */
 			}else{
-				state_turn90 = gen_initState;
-				return ERR_FAILED;
+				if(dir == left){
+					Maze_seg.segments[Maze_seg.numberOfSegments].SingleSegment = 900;
+				}else if(dir == right){
+					Maze_seg.segments[Maze_seg.numberOfSegments].SingleSegment = -900;
+				}else{
+					state_turn90 = gen_initState;
+					return ERR_FAILED;
+				}
+				Maze_seg.numberOfSegments = ++(*segmentNumber);
 			}
-			Maze_seg.segments[1].SingleSegment = 0; /* stop after this */
 			if(exploreDriving(Maze_seg, &adc_data)){
 				state_turn90 = gen_initState;
 				return ERR_FAILED;
@@ -201,6 +216,10 @@ byte turn90(uint8_t* segmentNumber, t_directions* currentOrientation, t_dir dir)
 
 }
 
+byte turn90(uint8_t* segmentNumber, t_directions* currentOrientation, t_dir dir){
+	turn90Intern(segmentNumber, currentOrientation, dir, true);
+}
+
 
 byte turn180(uint8_t* segmentNumber, t_directions* currentOrientation, t_dir dir){
 	static t_genericState state_turn180 = gen_initState;
@@ -209,7 +228,7 @@ byte turn180(uint8_t* segmentNumber, t_directions* currentOrientation, t_dir dir
 
 	switch(state_turn180){
 	 	 case gen_initState: /* is not used as init -> do ing 90° turn */
-			 switch(turn90(segmentNumber, currentOrientation, dir)){
+			 switch(turn90Intern(segmentNumber, currentOrientation, dir, true)){
 				 case ERR_BUSY:
 					 state_turn180 = gen_initState;
 					 break;
@@ -222,7 +241,7 @@ byte turn180(uint8_t* segmentNumber, t_directions* currentOrientation, t_dir dir
 			 }
 			 break;
 		 case gen_runnigState: /* turn again 90° */
-			 switch(turn90(segmentNumber, currentOrientation, dir)){
+			 switch(turn90Intern(segmentNumber, currentOrientation, dir, false)){
 				 case ERR_BUSY:
 					 state_turn180 = gen_runnigState;
 					 break;
