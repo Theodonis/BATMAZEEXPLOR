bdc4b671b69d54b82c15079eb58153fa34612470
Theodonis <theo.zwahlen@mail.ch>
2019-11-26 19:00:03 +0100

    Added config define

--- MCCAR_V_2_75/Sources/TargetInField_Position.h
+++ MCCAR_V_2_75/Sources/TargetInField_Position.h
@@ -16,7 +16,7 @@
 #define HALF_OF_MAZE_FIELD_LENGTH 		(0.095)	//(0.085)
 #define QUARTER_OF_MAZE_FIELD_LENGTH 	(0.0475)//(0.0425)
 #define INIT_POS_INFIELD 				(0.078)		//(0.074)
-#define POS_INFIELD_AFTER_TURN_180 		(0.075)//not yet done...		//(0.075)
+#define POS_INFIELD_AFTER_TURN_180 		(0.0777)//onely in one measure	//(0.075)//e...		//(0.075)
 #define POS_INFIELD_AFTER_TURN_90 		(0.0725)//not yet done...
 
 typedef enum fieldstate{
