94388bab9f36ca186d1db69e9ab26af737453593
Theodonis <theo.zwahlen@mail.ch>
2019-11-27 17:17:31 +0100

    Some config and add define for decide where log should be done

--- MCCAR_V_2_75/Sources/ExploreConfig.h
+++ MCCAR_V_2_75/Sources/ExploreConfig.h
@@ -16,9 +16,12 @@
 #define ENABLE_TIMING_CONROLL				(1&&ENABLE_EXPLORE_DATALOG)
 #endif
 
+#define LOG_DEPENDING_ON_CYCLE 				(1&&ENABLE_EXPLORE_DATALOG)
+#define LOG_DEPENDING_MAZE_POS 				(1&&ENABLE_EXPLORE_DATALOG&&!LOG_DEPENDING_ON_CYCLE)
+
 
 #define EXPLOR_DRIVE_TIME_AFTER_BRANCHDETECT 				(0.03) 	//time in s ~>additional driven m if velocity 1m/s
-#define EXPLOR_DRIVE_TIME_IN_KNOWN_FIELD_TO_STOPP_MIDDLED 	(0.095) //time in s ~>additional driven m if velocity 1m/s
+#define EXPLOR_DRIVE_TIME_IN_KNOWN_FIELD_TO_STOPP_MIDDLED 	(0.085)//(0.095) //time in s ~>additional driven m if velocity 1m/s
 #define MAX_DIST_TO_WALL_MM									(75) 	// if side wards distance is bigger ,means no wall there
 
 #define MAZE_FIELDS_WIDTH_NORTH_DIRECTION (9)
